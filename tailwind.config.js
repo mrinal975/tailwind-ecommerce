df72a4fdaebb4ca6c4cfb002f8e75e6fcc658d9e
Mrinal Mallik <mrinal@nagorik.tech>
2023-06-13 23:11:16 +0600

    taliwind css config edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extends: {},
+    extends: {
+      screen: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+      },
+      colors: {
+        "color-primary": "red",
+      },
+    },
   },
   Plugin: [],
 };
